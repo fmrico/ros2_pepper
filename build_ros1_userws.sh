9b37ccf43dc581d976ecae583c47093a8aeaa363
jgines <jonathangines@hotmail.com>
2018-03-15 16:12:42 +0100

    -pkg option added to build_user_ws

--- build_ros1_userws.sh
+++ build_ros1_userws.sh
@@ -13,7 +13,29 @@ PYTHON3_VERSION=${PYTHON3_MAJOR_VERSION}.${PYTHON3_MINOR_VERSION}.${PYTHON3_PATC
 INSTALL_ROOT=System
 
 set -eu -o pipefail
-set -xv
+
+package=""
+package_option=""
+workspace=""
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    --pkg|--package)
+    package="$2"
+    shift
+    shift
+    ;;
+    -w|--workspace)
+    workspace="$2"
+    shift
+    shift
+    ;;
+esac
+done
 
 if [ -z "$ALDE_CTC_CROSS" ]; then
   echo "Please define the ALDE_CTC_CROSS variable with the path to Aldebaran's Crosscompiler toolchain"
@@ -23,25 +45,30 @@ fi
 if [ ! -d User ]; then
   mkdir -p User
 fi
-cp -rf $1 User
-cd User
 
+if [ "$package" == ""  ]; then
+  cp -rf $workspace User
+else
+  package_option="--pkg $package"
+fi
+
+cd User
 for i in *_ws
 do
-	echo "Processing $i"
-	cd $i
-	mkdir -p cmake
-	cp ../../ctc-cmake-toolchain.cmake .
-	cp ../../cmake/eigen3-config.cmake ./cmake/
-	SYSTEM_DIR=${PWD}/../..
+  echo "Processing $i"
+  cd $i
+  mkdir -p cmake
+  cp ../../ctc-cmake-toolchain.cmake .
+  cp ../../cmake/eigen3-config.cmake ./cmake/
+  SYSTEM_DIR=${PWD}/../..
 
-	docker run -it --rm \
+  docker run -it --rm \
   	-e PYTHON2_VERSION=${PYTHON2_VERSION} \
   	-e PYTHON2_MAJOR_VERSION=${PYTHON2_MAJOR_VERSION} \
   	-e PYTHON2_MINOR_VERSION=${PYTHON2_MINOR_VERSION} \
   	-e PYTHON3_VERSION=${PYTHON3_VERSION} \
   	-e ALDE_CTC_CROSS=/home/nao/ctc \
-		-e PKG_CONFIG_PATH=/home/nao/${INSTALL_ROOT}/ros1_dependencies/lib/pkgconfig \
+  	-e PKG_CONFIG_PATH=/home/nao/${INSTALL_ROOT}/ros1_dependencies/lib/pkgconfig \
   	-v ${SYSTEM_DIR}/Python-${PYTHON2_VERSION}-host:/home/nao/${INSTALL_ROOT}/Python-${PYTHON2_VERSION}:ro \
   	-v ${SYSTEM_DIR}/Python-${PYTHON2_VERSION}-host:/home/nao/${INSTALL_ROOT}/Python-${PYTHON2_VERSION}-host:ro \
   	-v ${SYSTEM_DIR}/${INSTALL_ROOT}/Python-${PYTHON2_VERSION}:/home/nao/${INSTALL_ROOT}/Python-${PYTHON2_VERSION}-pepper:ro \
@@ -60,7 +87,7 @@ do
     export PATH=/home/nao/${INSTALL_ROOT}/Python-${PYTHON2_VERSION}/bin:$PATH && \
     source ${INSTALL_ROOT}/setup_ros1_pepper.bash && \
     cd User/$i && \
-    catkin_make -DCMAKE_BUILD_TYPE=Release \
+    catkin_make $package_option -DCMAKE_BUILD_TYPE=Release \
     --cmake-args \
     -DWITH_QT=OFF \
     -DSETUPTOOLS_DEB_LAYOUT=OFF \
@@ -75,4 +102,4 @@ do
     -DCMAKE_FIND_ROOT_PATH=\"/home/nao/${INSTALL_ROOT}/Python-${PYTHON2_VERSION}-pepper;/home/nao/${INSTALL_ROOT}/ros1_dependencies;/home/nao/${INSTALL_ROOT}/ros1_inst;/home/nao/ctc\" \
     "
     cd ../
-done
+  done
