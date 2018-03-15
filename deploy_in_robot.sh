89a0d710197ab9efcf00381ac696ede161270c64
jgines <jonathangines@hotmail.com>
2018-03-15 15:39:09 +0100

    naoqi option added in deploy script

--- deploy_in_robot.sh
+++ deploy_in_robot.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 system=0
 user=0
+naoqi=0
 help="Use [s] option to deploy System dir and [u] option to deploy User dir, ./deploy_in_robot.sh -[s,u]"
-while getopts ":hsu" opt; do
+while getopts ":hsun" opt; do
     case "$opt" in
     s)
         system=1
         ;;
     u)  user=1
+        ;;
+    n)  naoqi=1
         ;;
     h)  echo $help
         exit 0
@@ -20,9 +23,11 @@ if [ "$system" == 1 ]; then
 fi
 if [ "$user" == 1 ]; then
   rsync -avzh ./User nao@pepper.local:~/
+fi
+if [ "$naoqi" == 1 ]; then
   naoqi_dir=$(find . -type d -name 'naoqi')
   rsync -avzh $naoqi_dir nao@pepper.local:~/
 fi
-if [ "$system" == 0 ] && [ "$user" == 0 ]; then
+if [ "$system" == 0 ] && [ "$user" == 0 ] && [ "$naoqi" == 0 ]; then
   echo $help
 fi
