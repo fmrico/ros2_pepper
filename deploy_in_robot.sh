da419243c48e376d48f01ece13190a7b982e70ae
jgines <jonathangines@hotmail.com>
2018-03-15 11:10:18 +0100

    deploy script added

---
+++ deploy_in_robot.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+system=0
+user=0
+help="Use [s] option to deploy System dir and [u] option to deploy User dir, ./deploy_in_robot.sh -[s,u]"
+while getopts ":hsu" opt; do
+    case "$opt" in
+    s)
+        system=1
+        ;;
+    u)  user=1
+        ;;
+    h)  echo $help
+        exit 0
+        ;;
+    esac
+done
+
+if [ "$system" == 1 ]; then
+  rsync -avzh ./System nao@pepper.local:~/
+fi
+if [ "$user" == 1 ]; then
+  rsync -avzh ./User nao@pepper.local:~/
+  naoqi_dir=$(find . -type d -name 'naoqi')
+  rsync -avzh $naoqi_dir nao@pepper.local:~/
+fi
+if [ "$system" == 0 ] && [ "$user" == 0 ]; then
+  echo $help
+fi
